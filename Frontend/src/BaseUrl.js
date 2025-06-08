9347903724f8f09b83494794100f607fc5e59438
krishna23tiwari <krishna23tiwari.kt@gmail.com>
2025-06-08 11:30:52 +0530

    new

--- Frontend/src/BaseUrl.js
+++ Frontend/src/BaseUrl.js
@@ -1,9 +1,4 @@
 const baseurl = 'https://mini-trello-1kja.onrender.com'
-<<<<<<< HEAD
 
 export default baseurl
 
-=======
-
-export default baseurl
->>>>>>> a3ac6bec6a9e8b737b62e3e0e585d6cedc07b65e
